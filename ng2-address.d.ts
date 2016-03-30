1df1b2de519a051fc9869552376d4339d8dcbb77
Tomas Klingen <tomas.klingen@gmail.com>
2016-03-30 11:57:55 +0200

    Pointed package file to correct lib ts definition file.

--- ng2-address.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/address-autocomplete.component';
-export * from './src/address';
-export * from './src/autocomplete-service';
-export * from './src/google-places-autocomplete.service';
-export * from './src/place-details';
-export * from './src/place-suggestion';
